6ac2c6a657938c8b2eac5521a3b3f1af78c581c1
Sulaiman <sulaimain@kobo360.com>
2020-04-19 16:29:17 +0100

    created cart item dorpdown and connected it to the store

--- src/components/Cart-Dropdown/cart-dropdown.component.jsx
+++ src/components/Cart-Dropdown/cart-dropdown.component.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import CustomButton from '../Custom-Button/custom-button.component';
+import { connect } from 'react-redux';
 
+import CartItem from '../Cart-Item/cart-item.component';
 import './cart-dropdown.styles.scss';
 
-const Cart = () => (
+const Cart = ({ cartItems }) => (
     <div className="cart-dropdown">
         <div className="cart-items">
-            
+            {
+                cartItems.map(cartItem => (
+                    <CartItem key={cartItem.id} item={cartItem}/>
+                ))
+            }
         </div>
         <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
 );
 
-export default Cart;
+const mapStateToProps = ({ cart: { cartItems }}) => ({
+    cartItems
+})
+export default connect(mapStateToProps)(Cart);
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,10 +1,11 @@
 export const addItemToCart = (cartItems, itemToAdd) => {
+    // check if the new item is in cart itmes in state
     const existingCartItem = cartItems.find(cartItem => cartItem.id === itemToAdd.id);
-    if(existingCartItem){
+    if(existingCartItem){ // if it is there
         return cartItems.map(cartItem => 
             cartItem.id === itemToAdd.id 
             ? {...cartItem, quantity: cartItem.quantity + 1}
             : cartItem)
-    }
+    } //else 
     return [...cartItems, {...itemToAdd, quantity: 1}]
 }
